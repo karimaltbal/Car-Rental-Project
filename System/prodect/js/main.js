70f4f877ed645fe0192cef9434f4bb9f26ba4341
karim-altbal <karimaltbal4@gmail.com>
2022-01-28 10:27:06 +0200

    Added my prpject

---
+++ System/prodect/js/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+var btn= document.getElementById("btn");
+
+btn.onclick=function(){
+    var i=1;
+if(i==1){
+   //window.open ("https://asd5742.blogspot.com/","Get Your Pictures");
+   location="file:///I:/Front%20End%20Developer/My_Projects/Project-2/System/reservation/reservation.html";
+}
+}
